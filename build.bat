afaa986989774a59b6b8920c6cc4b0d0dbfe515f
NotAura <andreypirogov24@gmail.com>
2022-05-02 13:41:30 +0300

    added names for all build, updated .gitignore

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true
-dotnet publish -r win-x86 -c Release /p:PublishSingleFile=true
-dotnet publish -r win-arm -c Release /p:PublishSingleFile=true
-dotnet publish -r osx-x64 -c Release /p:PublishSingleFile=true
-dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true
-dotnet publish -r linux-arm -c Release /p:PublishSingleFile=true
+dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true;AssemblyName=AziProxyWin64
+dotnet publish -r win-x86 -c Release /p:PublishSingleFile=true;AssemblyName=AziProxyWin32
+dotnet publish -r win-arm -c Release /p:PublishSingleFile=true;AssemblyName=AziProxyWinArm
+dotnet publish -r osx-x64 -c Release /p:PublishSingleFile=true;AssemblyName=AziProxyOSX64
+dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true;AssemblyName=AziProxyLinux64
+dotnet publish -r linux-arm -c Release /p:PublishSingleFile=true;AssemblyName=AziProxyLinuxArm
